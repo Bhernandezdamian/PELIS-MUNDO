0695726ffbfc5b95ef81df562a2ae4fa5752db53
Bhernandezdamian <123613751+Bhernandezdamian@users.noreply.github.com>
2025-02-03 01:33:18 -0600

    Commit inicial

--- buscador.js
+++ buscador.js
@@ -6,21 +6,48 @@ const resultsSection = document.querySelector('#results-section');
 // Lista de películas
 const movies = [
 	{
-		title: 'Ant Man and the Wasp Quantumania',
-		image: 'Ant-Man.jpeg',
-		link: 'Ant-Man.html',
+		title: 'The Swarm (2024)',
+		image: 'https://ia903404.us.archive.org/32/items/bHsXjvIehwPpEwUhxlMFdvWyZVf/__ia_thumb.jpg',
+		link: 'The Swarm (2024).html',
 	},
 	{
-		title: 'agentes 355',
-		image: 'Agentes 355.jpeg',
-		link: 'Agentes-355(2022).html',
+		title: 'Amenaza en el aire(2025)',
+		image: 'https://ia600409.us.archive.org/24/items/xnsxsms/__ia_thumb.jpg',
+		link: 'Amenaza en el aire (2025).html',
 	},
 	{
-		title: 'moonfall',
-		image: 'moonfal.jpeg',
-		link: 'Moonfall.html',
+		title: 'The Liberation Men',
+		image: 'https://ia600108.us.archive.org/16/items/jkjefjen/__ia_thumb.jpg',
+		link: 'The Liberation Men (2024).html',
 	},
 	// Agrega más objetos de películas aquí...
+	{
+		title: 'Homestead',
+		image: 'https://ia600807.us.archive.org/10/items/jxsxkjsj/__ia_thumb.jpg',
+		link: 'Homestead (2024).html',
+	},
+
+
+	{
+		title: 'El Robo Perfecto: Pantera',
+		image: 'https://via.placeholder.com/200x300.png?text=Movie+2',
+		link: 'El Robo Perfecto Pantera.html',
+	},
+
+	{   
+		title: 'All the Lost Ones  Todos los perdidos',
+		image: 'https://ia902303.us.archive.org/7/items/fbdfbd/__ia_thumb.jpg',
+		link: 'All the Lost Ones.html',
+	},
+	{
+		title: 'El Robo Perfecto: Pantera',
+		image: 'https://via.placeholder.com/200x300.png?text=Movie+2',
+		link: 'El Robo Perfecto Pantera.html',
+	},
+
+
+
+
 ];
 
 // Función para buscar películas
